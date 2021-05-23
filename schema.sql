ebe4cdce2f530e58c74be1d6b4d024a6628fa062
rafaellevissa <rafaellevissa@gmail.com>
2021-05-23 20:37:02 -0300

    🗃 database schema added

---
+++ schema.sql
@@ -0,0 +1,16 @@
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ horus /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE horus;
+
+DROP TABLE IF EXISTS contacts;
+CREATE TABLE `contacts` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `status` enum('0','1') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+INSERT INTO contacts(id,name,phone,status) VALUES(1,'Renan Almeida','71999674175','0'),(15,'Renan','71999999999','0'),(20,'Renan Almeida','9999','1'),(21,'Renan Almeida','99999','1');
